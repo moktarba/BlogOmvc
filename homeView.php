16ec6b15b7a67cfe9d53c96a408c53298fd53586
moktarba <moktarba@hotmail.fr>
2018-11-17 12:01:14 +0100

    create the mvc model

--- homeView.php
+++ homeView.php
@@ -16,12 +16,9 @@
       <span class="navbar-brand mb-0 h1">BlogOmvc</span>
     </nav>
     <div class="container">
-      <h1>Hello, world!</h1>
+      <h1>Liste des Articles</h1>
       <div class="jumbotron">
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
       </div>
 
       <div class="row">
@@ -32,6 +29,7 @@
               echo '<div class="post col-lg-6 float-md-left">';
               echo '<h3>'.$post['title'].' <span style="font-size:10px" class="badge badge-secondary">'.$post['date_created_fr'].'</span></h3>';
               echo '<p>'.$post['description'].'</p>';
+              echo '<button><a href=?action=post&id='.$post["id"].'>Détails </a></button><br>';
               echo '</div>';
             }
           ?>
@@ -46,7 +44,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <p></p>
     <footer>
-      <nav class="navbar fixed-bottom navbar-light bg-light">
+      <nav class="navbar bottom navbar-light bg-light">
         <a class="navbar-brand" href="#">Fixed bottom</a>
       </nav>
     </footer>
